ecf7d9ec58f32877ae21d5f614f6a2fd90b6f04a
Verdi March <verdimrc@users.noreply.github.com>
2021-06-04 17:19:05 +0800

    Remove placeholder function

--- src/energy_storage_system/__init__.py
+++ src/energy_storage_system/__init__.py
@@ -1,15 +1,6 @@
-from typing import TypeVar
-
 from gym.envs.registration import register
 
 register(
     id="MyBatt-v1",
     entry_point="energy_storage_system.envs:SimpleBattery",
 )
-
-T = TypeVar("T")
-
-
-def hello_world(s: T) -> T:
-    print("Hello world,", s)
-    return s
