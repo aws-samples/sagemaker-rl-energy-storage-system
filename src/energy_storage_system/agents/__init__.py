06272abfdcf62d8cb731fb42740c2d2f70b28474
Verdi March <verdimrc@users.noreply.github.com>
2021-06-06 22:22:55 +0800

    Move bsaeline agents to submodule energy_storage_system.agents

---
+++ src/energy_storage_system/agents/__init__.py
@@ -0,0 +1,1 @@
+from .baseline import MovingAveragePriceAgent, PriceVsCostAgent, RandomAgent
---
+++ src/energy_storage_system/agents/baseline.py
@@ -0,0 +1,64 @@
+from abc import ABC
+
+import numpy as np
+
+from ..envs import SimpleBattery
+
+
+class Agent(ABC):
+    pass
+
+
+class RandomAgent(Agent):
+    """Random agent."""
+
+    actions = (SimpleBattery.CHARGE, SimpleBattery.DISCHARGE, SimpleBattery.HOLD)
+
+    def get_action(self, state):
+        return np.random.choice(self.actions)
+
+
+class PriceVsCostAgent(Agent):
+    """What should be the initial initial energy costs?
+
+    Buy: electric price < electric cost
+    Sell: electric price > electric cost
+    """
+
+    def get_action(self, state):
+        electric_price = state[2]
+        electric_cost = state[1]
+
+        if electric_price > electric_cost:
+            action = SimpleBattery.DISCHARGE
+        elif electric_price < electric_cost:
+            action = SimpleBattery.CHARGE
+        else:
+            action = SimpleBattery.HOLD
+
+        return action
+
+
+class MovingAveragePriceAgent(Agent):
+    """
+    Buy: market price < past last x days average price
+    Sell: market price > past last x days average price
+    """
+
+    def __init__(self, days: int = 5) -> None:
+        if days < 1:
+            raise ValueError(f"Days must be > 0, but getting {days}")
+        self.days = days
+
+    def get_action(self, state):
+        market_price = state[2]
+        past_average_price = sum(state[-self.days :]) / len(state[-self.days :])
+
+        if market_price > past_average_price:
+            action = SimpleBattery.DISCHARGE
+        elif market_price < past_average_price:
+            action = SimpleBattery.CHARGE
+        else:
+            action = SimpleBattery.HOLD
+
+        return action
