2b2db95afb5c52148538fefe98031da9a90535f8
Verdi March <verdimrc@users.noreply.github.com>
2021-06-08 13:45:29 +0800

    Docstring on source_dir/train_battery_sm.py

--- src/source_dir/train_battery_sm.py
+++ src/source_dir/train_battery_sm.py
@@ -3,13 +3,28 @@ from sagemaker_rl.ray_launcher import SageMakerRayLauncher
 
 
 class MyLauncher(SageMakerRayLauncher):
+    """Battery optimization using Ray-RLLib on SageMaker.
+
+    By default, PyTorch is used. However, TensorFlow can be chosen by setting SageMaker
+    estimator's hyperparameter ``"rl.training.config.use_pytorch"`` to ``False``.
+
+    See also: :class:`sagemaker_rl.ray_launcher.SageMakerRayLauncher`.
+    """
+
     def register_env_creator(self):
+        """Register a battery gym environment.
+
+        See also: :meth:`~sagemaker_rl.ray_launcher.SageMakerRayLauncher.register_env_creator`.
+        """
         from energy_storage_system.envs import SimpleBattery
 
         register_env("SimpleBattery-v1", lambda env_config: SimpleBattery(env_config))
 
     def get_experiment_config(self):
-        multi = 1
+        """Get the default configuration, which will be overriden by hyperparameters.
+
+        See also: :meth:`~sagemaker_rl.ray_launcher.SageMakerRayLauncher.get_experiment_config`.
+        """
         return {
             "training": {
                 "env": "SimpleBattery-v1",
